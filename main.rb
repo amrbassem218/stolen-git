e4fbced2984757daca7611c76ddff045afabbe7b
Amr Bassem ElTaweel <amrbeducation@gmail.com>
2026-03-11 14:55:16 +0200

    added basic init function

--- main.rb
+++ main.rb
@@ -1,13 +1,14 @@
 require 'optparse'
-
+require 'fileutils'
 command = ARGV.shift
 def print_usage
-  usage_error_message = "Usage: mygit <command> [options]
+  usage_error_message = "Usage: stolen-git <command> [options]
 
   Commands:\n"
   commands_docs = {
     commit: 'Save changes',
-    reset: 'Revert to commit'
+    reset: 'Revert to commit',
+    init: 'Initialize stolen-git to start tracking'
   }
   max_len = 0
   commands_docs.each_key do |command|
@@ -20,15 +21,60 @@ def print_usage
   puts usage_error_message
 end
 
+def initialize
+  if File.exist?('.stolen-git')
+    loop do
+      print 'An instance of stolen-git is already up here do you want to replace it (y/n): '
+      replace_user_input = gets.chomp.downcase
+      case replace_user_input
+      when 'y'
+        loop do
+          print 'THIS WILL DELETE ALL COMMITS AND INSTANCES OF STOLEN-GIT. ARE YOU SURE (y/n): '
+          confirmation_user_input = gets.chomp.downcase
+          case confirmation_user_input
+          when 'y'
+            FileUtils.rm_rf('.stolen-git')
+            if File.exist?('.stolen-git')
+              puts 'An error occured during the deletion process of the old directory of stolen-git'
+            else
+              initialize
+            end
+            break
+          when 'n'
+            puts 'ok'
+            break
+          else
+            puts 'wrong input only enter y/n'
+          end
+        end
+        break
+      when 'n'
+        puts 'ok'
+        break
+      else
+        puts 'wrong input only enter y/n'
+      end
+    end
+  else
+    Dir.mkdir('.stolen-git')
+
+  end
+end
+
 case command
-when 'push'
-  puts 'Pushing code...'
+when 'init'
+  initialize
+  puts 'Stolen-git initzlized Sucessfully :D'
 
 when 'commit'
   puts 'Committing changes...'
 
-when 'pull'
-  puts 'Pulling latest changes...'
+when 'reset'
+  puts 'Reverting to latest changes...'
+
+when 'help'
+  puts print_usage
+  exit 1
 
 when nil
   puts print_usage

b8e678d3baded39f951d8fb1b2199342d9d1e7c5
Amr Bassem ElTaweel <amrbeducation@gmail.com>
2026-04-24 17:43:19 +0300

    finished migration to new index

--- main.rb
+++ main.rb
@@ -79,82 +79,98 @@ def get_file_hash(path)
 end
 
 class DiffCalc
-  attr_reader :mem, :cnt
+  attr_reader :mem, :cnt, :old_s, :new_s
 
   def initialize
     @mem = {}
-    @cnt = 1
+    @cnt = 0
   end
 
-  def differencing(old_s, new_s, i1 = 0, i2 = 0)
-    return @mem[[i1, i2]] unless @mem[[i1, i2]].nil?
-    if i1 >= old_s.length && i2 >= new_s.length
-      return { insertion_seq: {}, deletion_seq: [], insertions: 0,
-               deletions: 0, diff_cnt: 0 }
+  def compute_diff(old_arr, new_arr)
+    @old_s = old_arr
+    @new_s = new_arr
+    differencing(0, 0)
+  end
+
+  def differencing(i1 = 0, i2 = 0)
+    cached = @mem[[i1, i2]]
+    return cached if cached
+
+    if i1 >= @old_s.length && i2 >= @new_s.length
+      result = { diff_cnt: 0, insertions: 0, deletions: 0, action: :done }
+      @mem[[i1, i2]] = result
+      return result
     end
 
-    if i1 >= old_s.length
-      trailing_ins = new_s.length - i2
-      insertion_seq = {}
-      (i2...new_s.length).each do |i|
-        insertion_seq[i] = {
-          value: new_s[i],
-          old_index: i1
-        }
+    if i1 >= @old_s.length
+      result = { diff_cnt: @new_s.length - i2, insertions: @new_s.length - i2, deletions: 0, action: :insert }
+      @mem[[i1, i2]] = result
+      return result
+    end
+
+    if i2 >= @new_s.length
+      result = { diff_cnt: @old_s.length - i1, insertions: 0, deletions: @old_s.length - i1, action: :delete }
+      @mem[[i1, i2]] = result
+      return result
+    end
+
+    if @old_s[i1] == @new_s[i2]
+      result = differencing(i1 + 1, i2 + 1)
+      result = { diff_cnt: result[:diff_cnt], insertions: result[:insertions], deletions: result[:deletions],
+                 action: :keep }
+      @mem[[i1, i2]] = result
+      return result
+    end
+
+    del = differencing(i1 + 1, i2)
+    add = differencing(i1, i2 + 1)
+
+    del_cost = del[:diff_cnt] + 1
+    add_cost = add[:diff_cnt] + 1
+
+    result = if del_cost <= add_cost
+               { diff_cnt: del_cost, insertions: del[:insertions], deletions: del[:deletions] + 1, action: :delete }
+             else
+               { diff_cnt: add_cost, insertions: add[:insertions] + 1, deletions: add[:deletions], action: :insert }
+             end
+
+    @mem[[i1, i2]] = result
+    result
+  end
+
+  def build_sequences
+    i1 = 0
+    i2 = 0
+    insertion_seq = {}
+    deletion_seq = {}
+
+    while i1 < @old_s.length || i2 < @new_s.length
+      action = @mem[[i1, i2]][:action]
+
+      case action
+      when :done
+        break
+      when :keep
+        i1 += 1
+        i2 += 1
+      when :delete
+        deletion_seq[i2] ||= []
+        deletion_seq[i2].push({ old_index: i1, ma_type: 'bs' })
+        i1 += 1
+      when :insert
+        insertion_seq[i2] = { value: @new_s[i2], old_index: i1 }
+        i2 += 1
       end
-      return { insertions: trailing_ins, deletions: 0, insertion_seq: insertion_seq,
-               deletion_seq: [], diff_cnt: trailing_ins }
-
-    elsif i2 >= new_s.length
-      trailing_del = old_s.length - i1
-      deletion_seq = (i1...old_s.length).to_a
-
-      return { insertions: 0, deletions: trailing_del, insertion_seq: {},
-               deletion_seq: deletion_seq, diff_cnt: trailing_del }
     end
 
-    default_path = { diff_cnt: 2**63 - 1, insertion_seq: {}, deletion_seq: [], insertions: 0, deletions: 0 }
-    min_path = default_path
-
-    if old_s[i1] == new_s[i2]
-      keep = differencing(old_s, new_s, i1 + 1, i2 + 1)
-      min_path = keep if keep && (keep[:insertions] + keep[:deletions] < min_path[:diff_cnt])
-    else
-      del = differencing(old_s, new_s, i1 + 1, i2)
-      add = differencing(old_s, new_s, i1, i2 + 1)
-
-      [del, add].each do |path|
-        next unless path
-
-        next unless path[:insertions] + path[:deletions] < min_path[:diff_cnt]
-
-        is_add = path == add
-        insertions = is_add ? path[:insertions] + 1 : path[:insertions]
-        deletions = is_add ? path[:deletions] : path[:deletions] + 1
-
-        insertion_seq = path[:insertion_seq].dup
-        deletion_seq = path[:deletion_seq].dup
-        if is_add
-          insertion_seq[i2] = {
-            value: new_s[i2],
-            old_index: i1
-          }
-        else
-          deletion_seq = [i1] + deletion_seq
-        end
-        min_path = {
-          diff_cnt: insertions + deletions,
-          insertions: insertions,
-          deletions: deletions,
-          insertion_seq: insertion_seq,
-          deletion_seq: deletion_seq
-        }
-      end
-
-    end
-    @mem[[i1, i2]] = min_path unless min_path == default_path
-    @cnt += 1
-    min_path
+    result = @mem[[0, 0]]
+    {
+      insertion_seq: insertion_seq,
+      deletion_seq: deletion_seq,
+      insertions: result[:insertions],
+      deletions: result[:deletions],
+      diff_cnt: result[:diff_cnt]
+    }
   end
 end
 
@@ -188,26 +204,18 @@ when 'diff'
     file_a = File.read(files[0]).split("\n")
     file_b = File.read(files[1]).split("\n")
     diff_calc = DiffCalc.new
-    diff = diff_calc.differencing(file_a, file_b)
+    diff_calc.compute_diff(file_a, file_b)
+    diff = diff_calc.build_sequences
     insertions, deletions, insertion_seq, deletion_seq = diff.values_at(:insertions, :deletions, :insertion_seq,
                                                                         :deletion_seq)
-
-    # debug
-    # puts '**FILE A ***'
-    # print file_a
-    # puts
-    #
-    # puts '**FILE B ***'
-    # print file_b
-    # puts
-    #
-    # puts '** Diff***'
-    # puts diff
-    # puts
-
     # sorting insertion_seq by keys
     insertion_seq = insertion_seq.sort.to_h
     insertion_seq_keys = insertion_seq.keys
+
+    deletion_seq = deletion_seq.sort.to_h
+    deletion_seq_keys = deletion_seq.keys
+    puts "deletion_seq_keys: #{deletion_seq_keys}"
+
     puts "Diff: (+): #{insertions}    (-): #{deletions}"
 
     # printing diff
@@ -218,22 +226,30 @@ when 'diff'
     # value (styled text to be printed)
     # type (-1 => deletion, 0 => insertion, 1 => not changed)
     edit_list = []
-    while insertion_poniter < insertions || deletion_pointer < deletions
+    while insertion_poniter < insertions || deletion_pointer < deletion_seq_keys.length
       is_insertion = true
       is_insertion = if insertion_poniter >= insertions
                        false
-                     elsif deletion_pointer >= deletions
+                     elsif deletion_pointer >= deletion_seq_keys.length
                        true
                      else
-                       insertion_seq_keys[insertion_poniter] < deletion_seq[deletion_pointer]
+                       insertion_seq_keys[insertion_poniter] < deletion_seq_keys[deletion_pointer]
                      end
-      index = is_insertion ? insertion_seq_keys[insertion_poniter] : deletion_seq[deletion_pointer]
+      index = is_insertion ? insertion_seq_keys[insertion_poniter] : deletion_seq_keys[deletion_pointer]
       if is_insertion
         edit_list.push({ index: index, value: "#{insertion_seq[index][:value]}", type: 0,
                          old_index: insertion_seq[index][:old_index] })
         insertion_poniter += 1
       else
-        edit_list.push({ index: index, value: "#{file_a[index]}", type: -1, old_index: index })
+        # puts "index: #{index}   deletion_pointer:#{deletion_pointer}  deletion_seq_keys:#{deletion_seq_keys}  deletions:#{deletions}  insertion_pointer:#{insertion_poniter} insertions:#{insertions}"
+        # puts "index: #{index}   deletion_ponter: #{deletion_pointer}    seq_keys_len:#{deletion_seq_keys.length}    "
+        deletion_seq[index] = deletion_seq[index].sort_by { |del| del[:old_index] }
+        deletion_seq[index].each do |del|
+          old_index = del[:old_index]
+          puts "bs: #{file_a[old_index]}   i: #{index}   old_index: #{old_index}" if del[:ma_type] == 'bs'
+          edit_list.push({ index: index, value: "#{file_a[old_index]}", type: -1,
+                           old_index: old_index })
+        end
         deletion_pointer += 1
       end
 
@@ -248,21 +264,32 @@ when 'diff'
         next unless j >= 0 && j < file_b.length
 
         line = order[:index] == j ? order : { index: j, value: file_b[j], type: 1, old_index: j }
-        print_queue[line[:old_index]] ||= []
-        print_queue[line[:old_index]].push(line) unless line[:type] == 1 && print_queue[j].length.positive?
+        print_queue[j] ||= []
+        print_queue[j].push(line) unless line[:type] == 1 && print_queue[j].length.positive?
       end
     end
 
     print_queue = print_queue.sort.to_h
     print_queue_keys = print_queue.keys
     printed = {}
+    # puts 'edit_list: '
+    # edit_list.each { |e| puts e }
+    #
+    # puts 'print_queue: '
+    # print_queue.each { |_k, v| puts v }
     def print_line(line)
       sign = if line[:type] == -1
                '-'
              else
                line[:type] == 0 ? '+' : ''
              end
-      print_text = "#{line[:index]} #{sign}#{line[:value]}"
+
+      index = if line[:type] == -1
+                line[:old_index]
+              else
+                line[:index]
+              end
+      print_text = "#{index} #{sign}#{line[:value]}"
       if line[:type] == -1
         print_text = print_text.red
       elsif line[:type] == 0
@@ -286,12 +313,9 @@ when 'diff'
         j = queue_i + 1
 
         define_method :is_deletion? do
-          is_within_range = j <= print_queue_keys.length
+          return false if j >= print_queue_keys.length
 
-          has_deletion = print_queue[print_queue_keys[j]].find do |x|
-            x[:type] == -1
-          end
-          is_within_range && has_deletion
+          print_queue[print_queue_keys[j]]&.any? { |x| x[:type] == -1 }
         end
 
         while is_deletion?
@@ -336,7 +360,8 @@ when 'test'
   end
   s1 = s1.split("\n")
   s2 = s2.split("\n")
-  diff = calc.differencing(s1, s2)
+  calc.compute_diff(s1, s2)
+  diff = calc.build_sequences
 
   puts ''
 
@@ -358,7 +383,7 @@ when 'test'
   #   puts "#{key}: #{val}"
   # end
   # test_sub = { bro: 'hi' }.merge(key => 'hey')
-  # print "test_seq: #{test_sub}"
+  # ; print "test_seq: #{test_sub}"
 
 when 'stage'
   files = ARG[1..]
